7184dfaef28f6527cf5cb5ba5d5b59336115a234
Ovik10 <svobodajindrich@centrum.cz>
2023-04-07 09:20:06 +0200

    adding google maps

---
+++ look/lib/reusable_widgets/my_drawer.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class MyDrawer extends StatelessWidget {
+  final VoidCallback? onHomeTap;
+  final VoidCallback? onSettingsTap;
+  final VoidCallback? onLogoutTap;
+
+  const MyDrawer({
+    Key? key,
+    this.onHomeTap,
+    this.onSettingsTap,
+    this.onLogoutTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          DrawerHeader(
+            decoration: BoxDecoration(
+              color: Colors.blue,
+            ),
+            child: Text(
+              'My App',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 24,
+              ),
+            ),
+          ),
+          ListTile(
+            leading: Icon(Icons.home),
+            title: Text('Home'),
+            onTap: onHomeTap,
+          ),
+          ListTile(
+            leading: Icon(Icons.settings),
+            title: Text('Settings'),
+            onTap: onSettingsTap,
+          ),
+          ListTile(
+            leading: Icon(Icons.logout),
+            title: Text('Logout'),
+            onTap: onLogoutTap,
+          ),
+        ],
+      ),
+    );
+  }
+}
--- look/lib/screens/home_screen.dart
+++ look/lib/screens/home_screen.dart
@@ -1,25 +1,30 @@
+import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart';
-import 'package:look/screens/signin_screen.dart';
+import 'package:look/reusable_widgets/my_drawer.dart';
+import 'package:look/screens/map.dart';
 import 'package:look/screens/signin_screen.dart';
 
 class HomeScreen extends StatefulWidget {
-
-
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          child: Text("Logout"),
-          onPressed: () {
-            FirebaseAuth.instance.signOut().then((value) {
+
+  void navigateToHome() {
+    Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => HomeScreen()),
+              );
+  }
+
+  void navigateToSettings() {
+    // Navigate to settings page
+  }
+
+  void logoutUser() {
+    FirebaseAuth.instance.signOut().then((value) {
               print("Signed Out");
               Navigator.push(
                 context,
@@ -28,9 +33,44 @@ class _HomeScreenState extends State<HomeScreen> {
             }).onError((error, stackTrace) {
               print("Error ${error.toString()}");
             });
-          },
+          }
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Look After'),
+      ),
+      drawer: MyDrawer(
+        onHomeTap: navigateToHome,
+        onSettingsTap: navigateToSettings,
+        onLogoutTap: logoutUser,
+      ),
+      body: Container(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.fromLTRB(20, MediaQuery.of(context).size.height * 0.05, 20, 0), 
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text('Map',
+                  style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 24, 
+                ),),
+          SizedBox(height: 20),
+          ElevatedButton(
+            child: Text('Show Map'),
+            onPressed: () {
+               Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => MapSample()),
+              );
+            },
+          ), ]
         ),
       ),
-    );
+    ),),);
   }
 }
